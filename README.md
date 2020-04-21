08d00cfaa0a7f35b5adc3086d720ee16d0395a0c
unknown <wetmanz23@gmail.com>
2020-04-21 18:09:58 +0700

    add convert to excel, bugfixing

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ This example code was implemented using Page Object Pattern.
 Tech used:
 - Python 3
 - Selenium
+- Pandas
+- openpyxl
 
 Usage: python scrap_ml_reviews.py
--- scrap_ml_reviews.py
+++ scrap_ml_reviews.py
@@ -1,10 +1,11 @@
 from action import MLPlayStoreAction
+import pandas as pd
 import json
 import time
 
 action = MLPlayStoreAction()
-total_reviews = 100
-scroll_delay = 1
+total_reviews = 10000
+scroll_delay = 0.5
 user_reviews = []
 
 action.user_open_website()
@@ -15,10 +16,11 @@ while True:
     start_time = time.time()
 
     action.user_scroll_to_bottom()
-
     time.sleep(scroll_delay)
     action.user_click_show_more_button()
     new_user_reviews = action.user_get_all_reviews(old_count)
+    time.sleep(scroll_delay)
+    action.user_click_show_more_button()
 
     end_time = time.time()
     new_count = old_count + len(new_user_reviews)
@@ -37,13 +39,17 @@ action.user_close_website()
 
 print("GET Last {} reviews...".format(total_reviews))
 user_reviews = user_reviews[0:total_reviews]
-print("Saving data to JSON format...")
 data = {
     'reviews': user_reviews
 }
+
+print("Saving data to JSON format...")
 f = open('mobile_legend_playstore_reviews.json', 'w')
 f.write(json.dumps(data, indent = 4))
 f.close()
+
+print("Saving data to EXCEL format...")
+pd.read_json("mobile_legend_playstore_reviews.json").to_excel("mobile_legend_playstore_reviews.xlsx")
 
 print("DONE~")
 
